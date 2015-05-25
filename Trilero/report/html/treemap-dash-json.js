90b4b120fc93daef5be9a17c5e1349b5f5cb6046
Eric <Eric@Eric-PC>
2015-05-25 06:36:09 +0200

    modelio

---
+++ Trilero/report/html/treemap-dash-json.js
@@ -0,0 +1,7 @@
+var treeMapJson = {"id":"Clover database lun may 25 2015 03:00:58 CEST0","name":"","data":{
+    "$area":195.0,"$color":97.948715,"title":
+    " 195 Elements, 97,9% Coverage"},"children":[{"id":"default-pkg0","name":
+      "default-pkg","data":{"$area":195.0,"$color":97.948715,"title":
+        "default-pkg 195 Elements, 97,9% Coverage"},"children":[]}]}
+;
+processTreeMapDashJson (treeMapJson);
---
+++ Trilero/report/html/treemap-json.js
@@ -0,0 +1,10 @@
+var treeMapJson = {"id":"Clover database lun may 25 2015 03:00:58 CEST0","name":"","data":{
+    "$area":195.0,"$color":97.948715,"title":
+    " 195 Elements, 97,9% Coverage"},"children":[{"id":"default-pkg0","name":
+      "default-pkg","data":{"$area":195.0,"$color":97.948715,"title":
+        "default-pkg 195 Elements, 97,9% Coverage"},"children":[{"id":
+          "Trilero0","name":"Trilero","data":{"$area":195.0,"$color":
+            97.948715,"path":"default-pkg/Trilero.html#Trilero","title":
+            "Trilero 195 Elements, 97,9% Coverage"},"children":[]}]}]}
+;
+processTreeMapJson (treeMapJson);
